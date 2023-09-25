038e129175d8038664963294c135fd9aa8868d63
VENGENCE7 <bhavish007anand@gmail.com>
2023-09-25 16:32:46 +0530

    backend changes ,code formated better

--- Backend/core/utils/getFlights.py
+++ Backend/core/utils/getFlights.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 import requests
 
-
 def get_flight_in_period(fly_from, fly_to, date_from, date_to, sort):
     try:
         headers = {"apikey": "0tvfUPHxSK3axHyPv0wOlCKPcwcF-T7f"}
+
         BASE_URL = f'https://api.tequila.kiwi.com/v2/search?fly_from={fly_from}&fly_to={fly_to}&date_from={date_from}&date_to={date_to}&sort={sort}&limit=5&curr=AUD'
+
         response = requests.get(BASE_URL, headers=headers)
         response = response.json()
         flights = []
         for flight in response['data']:
+
             price = flight['price']
             duration = flight['duration']['departure']/3600
             availability = flight['availability']["seats"]
@@ -17,6 +19,7 @@ def get_flight_in_period(fly_from, fly_to, date_from, date_to, sort):
             departure= datetime.fromisoformat(flight['local_departure'][:-1])
             arrival=datetime.fromisoformat(flight['local_arrival'][:-1])
             details = []
+
             for idx, route in enumerate(routes):
                 singleRoute={
                     "index":idx,
@@ -35,7 +38,9 @@ def get_flight_in_period(fly_from, fly_to, date_from, date_to, sort):
                 "arrival":arrival.strftime("%b %d, %Y %I:%M %p"),
                 "routes": details
             }
+
             flights.append(flight_info)
+            
         return flights
     except:
         print("An exception occurred")
